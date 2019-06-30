65615c2672471ec210d3247a9a099ea64558d012
Phumsan <52192362+Phumsan@users.noreply.github.com>
2019-06-30 19:25:54 +0700

    Add files via upload

---
+++ ex1/work/work1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+void main()
+{
+	int thaib;
+	cout << "Input Thai Baths : " ;
+	cin >> thaib;
+	cout << "****Exchange Rate****" << endl;
+	cout << thaib << " Baths is " << thaib*0.033 << " Dollars. " << endl;
+	cout << thaib << " Baths is " << thaib*0.029 << " Euro. " << endl;
+}
